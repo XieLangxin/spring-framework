04cb01d483c2a000ea2564a7aa399fefaba5ba7b
XieLangxin <867228087@qq.com>
2022-01-07 19:33:44 +0800

    aop注解

--- spring-lagou/src/main/java/com/lagou/SpringConfig.java
+++ spring-lagou/src/main/java/com/lagou/SpringConfig.java
@@ -4,29 +4,33 @@ import com.lagou.pojo.Company;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author XieLangxin
  * @date 2022/1/4
  */
+@ComponentScan({"com.lagou.service","com.lagou.utils"})
+@EnableAspectJAutoProxy
 @Configuration
 public class SpringConfig {
-	@Bean
-	public Company company(){
-		Company company = new Company();
-		company.setId(1);
-		company.setName("lagou");
-		return company;
-	}
+	//@Bean
+	//public Company company(){
+	//	Company company = new Company();
+	//	company.setId(1);
+	//	company.setName("lagou");
+	//	return company;
+	//}
 
-	@Bean
-	public BeanPostProcessor beanPostProcessor(){
-		return new MyBeanPostProcessor();
-	}
-
-	@Bean
-	public BeanFactoryPostProcessor beanFactoryPostProcessor(){
-		return new MyBeanFactoryPostProcessor();
-	}
+	//@Bean
+	//public BeanPostProcessor beanPostProcessor(){
+	//	return new MyBeanPostProcessor();
+	//}
+	//
+	//@Bean
+	//public BeanFactoryPostProcessor beanFactoryPostProcessor(){
+	//	return new MyBeanFactoryPostProcessor();
+	//}
 }
---
+++ spring-lagou/src/main/java/com/lagou/service/MyService.java
@@ -0,0 +1,9 @@
+package com.lagou.service;
+
+/**
+ * @author XieLangxin
+ * @date 2022/1/7
+ */
+public interface MyService {
+	void printHelloWorld();
+}
